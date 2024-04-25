2f35daac5e7be0f9fb1a7c35fd1627073802336f
Mustafa Taha Şahin <mustafatahasahin@gmail.com>
2024-04-25 20:58:44 +0300

    add documentation

--- lib.go
+++ lib.go
@@ -1,5 +1,7 @@
+// Package gomodule contains stuff that is not very interesting
 package gomodule
 
+// Add adds two numbers. To learn more about addition see https://www.mathsisfun.com/numbers/addition.html
 func Add(num1 int, num2 int) int {
 	return num1 + num2
 }
